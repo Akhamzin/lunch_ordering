04660cde2f830bf8710d971a399c25422447b926
Artyom Khamzin <akhamzin90@yandex.ru>
2017-01-16 11:14:45 -0800

    Complete unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+	describe "Custom field to Devise" do
+		it { should respond_to(:email) }
+	  	it { should respond_to(:name) }
+	end
 	describe "Associations" do
 		it { should belong_to(:role) }
 	end
